193e14b0b0b7a529711663cff43caa089251dd02
Josh Soref <jsoref@users.noreply.github.com>
2020-12-20 12:25:21 -0500

    spelling: multi
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- demos/buildah_multi_stage.sh
+++ demos/buildah_multi_stage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 # author : tsweeney (based on ipbabble's other demos) 
-# Based on Alex Ellis blog (https://blog.alexellis.io/mutli-stage-docker-builds)
+# Based on Alex Ellis blog (https://blog.alexellis.io/multi-stage-docker-builds)
 # Assumptions install buildah and podman
 # Set some of the variables below
 
